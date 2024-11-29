05289027d1a4c4d6fb859114a403c7b7873201d3
belinhaLIGHT <154287268+belinhaLIGHT@users.noreply.github.com>
2024-11-28 21:22:04 -0300

    Add files via upload

---
+++ scrioot.js
@@ -0,0 +1,24 @@
+function pag14() {
+    window.location = "3-1.html"
+}
+function pag15() {
+    window.location = "3-2.html"
+}
+function pag16() {
+    window.location = "3-3.html"
+}
+function pag17() {
+    window.location = "3-4.html"
+}
+function pag18() {
+    window.location = "3-5.html"
+}
+function pag19() {
+    window.location = "3-6.html"
+}
+function pag20() {
+    window.location = "3-7.html"
+}
+function pagmorrer3() {
+    window.location = "morrerlvl3.html"
+}
